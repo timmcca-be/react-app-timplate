529cd2b4215ba05cd6bba4b7f81e8724a43e17ac
Tim McCabe <timmccabe@khanacademy.org>
2022-09-20 11:29:45 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./App.module.css";
+
+export function App() {
+    return (
+        <div className={styles.app}>
+            <p>hello world</p>
+        </div>
+    );
+}
